8c47829e5c841f8a2e46a0f083cfbbab76484db1
karimkbu <karimk@bu.edu>
2020-05-05 20:58:08 +0000

    guess.feature scenarios pass

--- app.rb
+++ app.rb
@@ -40,6 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+        guess = @game.guess(letter)
+    end
     redirect '/show'
   end
   
